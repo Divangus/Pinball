62ea28eae41370c732765fbbca31d160c2738150
Xavierlm11 <xaviloma11@gmail.com>
2021-10-26 17:34:07 +0200

    a

--- Pinball/ModuleSceneIntro.cpp
+++ Pinball/ModuleSceneIntro.cpp
@@ -49,7 +49,7 @@ bool ModuleSceneIntro::Start()
 	//left
 	//left = App->physics->CreateRectangle(174, 939, 70, 25);
 
-
+	map();
 	return ret;
 }
 
@@ -64,25 +64,26 @@ bool ModuleSceneIntro::CleanUp()
 // Update: draw background
 update_status ModuleSceneIntro::Update()
 {
-	if(App->input->GetKey(SDL_SCANCODE_SPACE) == KEY_DOWN)
+
+	if (App->input->GetKey(SDL_SCANCODE_SPACE) == KEY_DOWN)
 	{
 		ray_on = !ray_on;
 		ray.x = App->input->GetMouseX();
 		ray.y = App->input->GetMouseY();
 	}
 
-	if(App->input->GetKey(SDL_SCANCODE_1) == KEY_DOWN)
+	if (App->input->GetKey(SDL_SCANCODE_1) == KEY_DOWN)
 	{
 		circles.add(App->physics->CreateCircle(App->input->GetMouseX(), App->input->GetMouseY(), 25));
 		circles.getLast()->data->listener = this;
 	}
 
-	if(App->input->GetKey(SDL_SCANCODE_2) == KEY_DOWN)
+	if (App->input->GetKey(SDL_SCANCODE_2) == KEY_DOWN)
 	{
 		boxes.add(App->physics->CreateRectangle(App->input->GetMouseX(), App->input->GetMouseY(), 100, 50));
 	}
 
-	if(App->input->GetKey(SDL_SCANCODE_3) == KEY_DOWN)
+	if (App->input->GetKey(SDL_SCANCODE_3) == KEY_DOWN)
 	{
 		// Pivot 0, 0
 		int rick_head[64] = {
@@ -122,138 +123,8 @@ update_status ModuleSceneIntro::Update()
 
 		ricks.add(App->physics->CreateChain(App->input->GetMouseX(), App->input->GetMouseY(), rick_head, 64));
 	}
-
-	
-		// Pivot 0, 0
-		int elephant[76] = {
-			1488, 3295,
-			1606, 3295,
-			1611, 937,
-			1502, 675,
-			1355, 440,
-			1182, 277,
-			1011, 197,
-			835, 168,
-			613, 215,
-			488, 291,
-			320, 477,
-			326, 597,
-			420, 622,
-			511, 604,
-			528, 631,
-			442, 757,
-			466, 902,
-			477, 1026,
-			420, 1091,
-			382, 1304,
-			326, 1304,
-			377, 1528,
-			284, 1160,
-			144, 1153,
-			266, 1615,
-			240, 1573,
-			108, 1815,
-			100, 2615,
-			684, 3095,
-			677, 3162,
-			591, 3268,
-			962, 3271,
-			877, 3164,
-			833, 3108,
-			1442, 2604,
-			1437, 1682,
-			1491, 1680,
-			1482, 3280
-		};
-
-		elephs.add(App->physics->CreateChain(0, 0, elephant, 76));
-	
-		int wallsup[46] = {
-			1434, 1682,
-			1360, 1534,
-			1323, 1506,
-			1348, 1287,
-			1325, 1188,
-			1339, 1156,
-			1369, 1055,
-			1402, 1011,
-			1411, 935,
-			1423, 866,
-			1380, 758,
-			1346, 720,
-			1263, 864,
-			1223, 825,
-			1296, 684,
-			1255, 656,
-			1254, 557,
-			1333, 650,
-			1390, 734,
-			1440, 809,
-			1473, 928,
-			1495, 1222,
-			1500, 1677
-		};
-
-		walls.add(App->physics->CreateChain(0, 0, wallsup, 46));
-
-		int wallRdw[12] = {
-			1336, 1963,
-			1340, 2515,
-			991, 2813,
-			1018, 2853,
-			1370, 2545,
-			1374, 1961
-		};
-		walls.add(App->physics->CreateChain(0, 0, wallRdw, 12));
-
-		int wallRup[12] = {
-			1219, 2090,
-			1220, 2372,
-			1059, 2510,
-			1075, 2534,
-			1249, 2389,
-			1249, 2090
-		};
-		walls.add(App->physics->CreateChain(0, 0, wallRup, 12));
-
-		int trinR[8] = {
-			978, 2309,
-			1121, 2044,
-			1141, 2227,
-			1021, 2332
-		};
-		walls.add(App->physics->CreateChain(0, 0, trinR, 8));
-
-		int trinL[8] = {
-			590, 2296,
-			448, 2018,
-			423, 2203,
-			560, 2306
-		};
-		walls.add(App->physics->CreateChain(0, 0, trinL, 8));
-
-		int wallLdw[12] = {
-			214, 1982,
-			217, 2576,
-			562, 2889,
-			600, 2846,
-			261, 2545,
-			261, 1986
-		};
-		walls.add(App->physics->CreateChain(0, 0, wallLdw, 12));
-
-		int wallLup[12] = {
-			329, 2114,
-			329, 2414,
-			505, 2565,
-			524, 2536,
-			361, 2405,
-			362, 2118
-		};
-		walls.add(App->physics->CreateChain(0, 0, wallLup, 12));
-
 	//background
-	App->renderer->Blit(background, 0, 0, NULL, 1.0f);
+	//App->renderer->Blit(background, 0, 0, NULL, 1.0f);
 	// Prepare for raycast ------------------------------------------------------
 	
 	iPoint mouse;
@@ -315,6 +186,197 @@ update_status ModuleSceneIntro::Update()
 	}
 
 	return UPDATE_CONTINUE;
+}
+
+void ModuleSceneIntro::map()
+{
+	
+
+
+	// Pivot 0, 0
+	/*int elephant[76] = {
+		1488, 3295,
+		1606, 3295,
+		1611, 937,
+		1502, 675,
+		1355, 440,
+		1182, 277,
+		1011, 197,
+		835, 168,
+		613, 215,
+		488, 291,
+		320, 477,
+		326, 597,
+		420, 622,
+		511, 604,
+		528, 631,
+		442, 757,
+		466, 902,
+		477, 1026,
+		420, 1091,
+		382, 1304,
+		326, 1304,
+		377, 1528,
+		284, 1160,
+		144, 1153,
+		266, 1615,
+		240, 1573,
+		108, 1815,
+		100, 2615,
+		684, 3095,
+		677, 3162,
+		591, 3268,
+		962, 3271,
+		877, 3164,
+		833, 3108,
+		1442, 2604,
+		1437, 1682,
+		1491, 1680,
+		1482, 3280
+	};
+
+	elephs.add(App->physics->CreateChain(0, 0, elephant, 76));*/
+	int elephant[106] = {
+			848, 3102,
+			1440, 2602,
+			1431, 1680,
+			1322, 1514,
+			1344, 1304,
+			1320, 1194,
+			1411, 991,
+			1422, 928,
+			1382, 765,
+			1348, 717,
+			1265, 857,
+			1225, 831,
+			1291, 691,
+			1257, 642,
+			1271, 600,
+			1385, 705,
+			1474, 880,
+			1485, 1331,
+			1502, 1794,
+			1504, 3156,
+			1608, 3156,
+			1616, 912,
+			1416, 508,
+			1204, 300,
+			1024, 196,
+			840, 176,
+			688, 188,
+			508, 272,
+			352, 428,
+			312, 520,
+			344, 612,
+			530, 613,
+			472, 708,
+			446, 826,
+			483, 930,
+			473, 1043,
+			413, 1110,
+			383, 1303,
+			330, 1310,
+			376, 1523,
+			280, 1156,
+			150, 1153,
+			270, 1616,
+			236, 1573,
+			103, 1826,
+			96, 2623,
+			676, 3093,
+			682, 3137,
+			34, 3111,
+			28, 51,
+			1640, 51,
+			1657, 3197,
+			871, 3197
+	};
+
+	elephs.add(App->physics->CreateChain(0, 0, elephant, 106));
+
+	int wallsup[46] = {
+		1434, 1682,
+		1360, 1534,
+		1323, 1506,
+		1348, 1287,
+		1325, 1188,
+		1339, 1156,
+		1369, 1055,
+		1402, 1011,
+		1411, 935,
+		1423, 866,
+		1380, 758,
+		1346, 720,
+		1263, 864,
+		1223, 825,
+		1296, 684,
+		1255, 656,
+		1254, 557,
+		1333, 650,
+		1390, 734,
+		1440, 809,
+		1473, 928,
+		1495, 1222,
+		1500, 1677
+	};
+
+	walls.add(App->physics->CreateChain(0, 0, wallsup, 46));
+
+	int wallRdw[12] = {
+		1336, 1963,
+		1340, 2515,
+		991, 2813,
+		1018, 2853,
+		1370, 2545,
+		1374, 1961
+	};
+	walls.add(App->physics->CreateChain(0, 0, wallRdw, 12));
+
+	int wallRup[12] = {
+		1219, 2090,
+		1220, 2372,
+		1059, 2510,
+		1075, 2534,
+		1249, 2389,
+		1249, 2090
+	};
+	walls.add(App->physics->CreateChain(0, 0, wallRup, 12));
+
+	int trinR[8] = {
+		978, 2309,
+		1121, 2044,
+		1141, 2227,
+		1021, 2332
+	};
+	walls.add(App->physics->CreateChain(0, 0, trinR, 8));
+
+	int trinL[8] = {
+		590, 2296,
+		448, 2018,
+		423, 2203,
+		560, 2306
+	};
+	walls.add(App->physics->CreateChain(0, 0, trinL, 8));
+
+	int wallLdw[12] = {
+		214, 1982,
+		217, 2576,
+		562, 2889,
+		600, 2846,
+		261, 2545,
+		261, 1986
+	};
+	walls.add(App->physics->CreateChain(0, 0, wallLdw, 12));
+
+	int wallLup[12] = {
+		329, 2114,
+		329, 2414,
+		505, 2565,
+		524, 2536,
+		361, 2405,
+		362, 2118
+	};
+	walls.add(App->physics->CreateChain(0, 0, wallLup, 12));
 }
 
 void ModuleSceneIntro::OnCollision(PhysBody* bodyA, PhysBody* bodyB)
